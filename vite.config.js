c9677f35b5d1a01aa2098887c8d58010300ebf1c
Bharathi Bharathi <itisbharathi20@gmail.com>
2026-03-24 12:07:57 +0530

    Update Deploying Issues

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/portfolio-website/', // <-- replace with your GitHub repo name
-  plugins: [react()],
-})
+export default {
+  base: "./"
+}
 
